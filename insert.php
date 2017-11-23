6fd9e340c5e96bdf60bd12bcc14b3b708c5073cc
BekkiHaruka <muses-c.8823@docomo.ne.jp>
2017-11-23 21:54:57 +0900

    Initial Commit

---
+++ insert.php
@@ -0,0 +1,42 @@
+<html>
+<meta charset="utf-8">
+<head>
+<title>ログインユーザー追加ページ</title>
+</head>
+<body>
+<?php
+$link = mysql_connect('ホスト名','ユーザー名','パスワード');
+if (!$link) {
+die('接続失敗です。'.mysql_error());
+}
+
+$db_selected = mysql_select_db('データベース名',$link);
+if(!$db_selected){
+die('データベース選択失敗です。'.mysql_error());
+}
+
+mysql_set_charset('utf8');
+
+$result = mysql_query('SELECT name,password FROM テーブル名');
+if(!$result){
+die('SELECTクエリーが失敗しました。'.mysql_error());
+}
+
+$name = $_POST['name'];
+$password = $_POST['password'];
+
+$sql = "INSERT INTO テーブル名 (name,password) VALUES ('$name','$password')";
+$result_flag = mysql_query($sql);
+
+if(!$result_flag){
+die('INSERTクエリーが失敗しました。すでに同じNAMEが登録されている可能性があります。<br><a href="add.html">戻る</a>');
+}
+
+print('<p>' . $name . 'ユーザーを登録しました。</p>');
+
+$close_flag = mysql_close($link);
+
+?>
+<a href="add.html">戻る</a>
+</body>
+</html>
